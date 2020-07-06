cbbdf5374c16487e4f84a1e6393153551aed935c
RTSTALER <rstaler@yandex.ru>
2020-07-06 13:27:10 +0700

    restore test case

--- src/tests/token.test.ts
+++ src/tests/token.test.ts
@@ -1,10 +1,11 @@
 import { expect } from 'chai';
 
+import config from '../../halva';
 import { init } from './../index';
 
 describe('Token runtime', () => {
   before(async () => {
-    this.env = await init('./../index');
+    this.env = await init(config);
   });
 
   describe('polkadot api', () => {
@@ -13,4 +14,11 @@ describe('Token runtime', () => {
     });
   });
 
+  describe('getting balances', () => {
+    it('should return correct balance', async () => {
+      this.env.accounts.map(async (account: any) => {
+        console.log(await this.env.polkadot.query.token.balances(account.address));
+      });
+    });
+  });
 });
