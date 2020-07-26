7479a8755c1fd624033a5354aec03a7c121b9b05
Dmitry Pavlov <bykeks92@gmail.com>
2020-07-26 12:31:33 +0700

    update packages

--- halva.js
+++ halva.js
@@ -1,10 +1,11 @@
-const mnemonic = "bottom drive obey lake curtain smoke basket hold race lonely fit walk";
+const mnemonic =
+  'bottom drive obey lake curtain smoke basket hold race lonely fit walk';
 
 module.exports = {
   networks: {
     test: {
       mnemonic,
-      ws: "ws://127.0.0.1:9944",
-    },
+      ws: 'ws://127.0.0.1:9944'
+    }
   }
-}
+};
