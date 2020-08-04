a6f3f212124025becf293043cdad78e547607d15
RTSTALER <rtstaler@yandex.ru>
2020-08-04 23:39:26 +0700

    Delete hardcode parametr

--- src/Configure/FindConfigFile.ts
+++ src/Configure/FindConfigFile.ts
@@ -2,8 +2,10 @@ import { existsSync } from 'fs';
 import { join } from 'path';
 
 export function getConfigureModule(filename: string): string {
+  if(!filename) {
+    filename = 'halva.js';
+  }
   const configPath = join(process.cwd(), filename);
-
   if (!existsSync(configPath)) {
     throw new Error(`Could not find suitable configuration file.`);
   }
